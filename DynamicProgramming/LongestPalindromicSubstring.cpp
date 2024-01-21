6bad30ec0b3cef46b31d162703e7cb52cccdae50
HN026 <plancksconstant01@gmail.com>
2024-01-21 20:24:10 +0530

    DP

---
+++ DynamicProgramming/LongestPalindromicSubstring.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+string LPS(string s){
+    int n = s.size();
+
+    vector<vector<int>> dp(n, vector<int> (n, 0));
+    int maxlen = 1;
+    
+    // For substring length 1
+    for(int i = 0; i<n; i++){
+        dp[i][i] = 1;
+    }
+
+    // For substring length 2
+    int start = 0;
+    for(int i = 0; i<n-1; i++){
+        if(s[i]==s[i+1]){
+            dp[i][i+1] = 1;
+            start = i;
+            maxlen = 2;
+        }
+    }
+
+    // For K length substring
+    for(int k = 3; k<=n; k++){
+        for(int i = 0; i<n-k+1; i++){
+            int j = i+k-1;
+            if(dp[i+1][j-1] && s[i]==s[j]){
+                dp[i][j] = 1;
+
+                if(k>maxlen){
+                    start = i;
+                    maxlen = k;
+                }
+            }
+        }
+    }
+
+    string ans = s.substr(start, start+maxlen-1);
+    return ans;
+}   
+
+int main(){
+    string s = "aaaabbaa";
+
+    string ans = LPS(s);
+    cout<<ans<<endl;
+    return 0; 
+}
---
+++ DynamicProgramming/MinimumFallingPathSum.cpp
@@ -0,0 +1,2 @@
+// In this question dp[i][j] represents basically the lowest cost to reach that particular block in the matrix. 
+// Find
